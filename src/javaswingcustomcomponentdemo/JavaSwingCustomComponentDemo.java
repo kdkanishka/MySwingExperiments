6cb16d94f039b7e0e972d6dd2beb64872303eef8
Kanishka <kdkanishka@gmail.com>
2013-12-19 15:52:23 +0530

    Initial commit

---
+++ src/javaswingcustomcomponentdemo/JavaSwingCustomComponentDemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswingcustomcomponentdemo;
+
+/**
+ *
+ * @author kanishka
+ */
+public class JavaSwingCustomComponentDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CustCompoDemo cd=new CustCompoDemo();
+        cd.setLocationRelativeTo(null);
+        cd.setVisible(true);
+    }
+}
